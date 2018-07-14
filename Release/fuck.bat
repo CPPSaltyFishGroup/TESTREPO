213bebfccbb1c3cd76e3c05844a53ab8e66fef9c
oxygen <937675166@qq.com>
2018-07-14 10:32:28 +0800

    to test the arguments

---
+++ Release/fuck.bat
@@ -0,0 +1,1 @@
+start TestProject.exe abcdefg
--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,10 +1,17 @@
 #include<cstdio>
-int main(int n,char*arg) {
+#include<cstdlib>
+#include<iostream>
+int main(int n,char*arg[]) {
 	int*p;// = &n;
 	//scanf_s("%d", &n);
-	if(n>1)
-		printf_s("hello world!");
+	char ok[30] = {};
+	//std::cin>> ok;
+	if (n>1) {
+		printf_s("hello world!  argment:%s",arg[1]);
+		//system("pause");
+	}
 	else {
+		//getchar();
 		p = 0x0000000000000000;
 		*p = 123;
 	}
