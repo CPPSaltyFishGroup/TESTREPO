232ae5ea0116221c26014d8b9cfa9696baabc6e3
oxygen <937675166@qq.com>
2018-07-16 09:18:26 +0800

    in the animation project, click can draw new a square

--- JumpAnimationExample/OpenCVTester/main.cpp
+++ JumpAnimationExample/OpenCVTester/main.cpp
@@ -6,11 +6,20 @@
 #include"Tools.h"
 
 using namespace cv;
-
+Mat box,canvas,back;
+void onMouse(int event, int x, int y, int type, void *param) {
+	if (event == EVENT_LBUTTONDOWN) {
+		Mat cache = back(Rect(
+			Tools::clamp(back.cols - box.cols, 0, x),
+			Tools::clamp(back.rows - box.rows, 0, y),
+			box.cols, box.rows));
+		addWeighted(cache, 0.5, box, 0.5, 0.0, cache);
+	}
+}
 int main() {
 	// 读入一张图片（游戏原画）    
-	Mat back = imread("pic.jpg");
-	Mat box = imread("box.jpg");
+	back = imread("pic.jpg");
+	box = imread("box.jpg");
 	Mat cache;
 	int height = 0;
 	float acceleration = 1, maxVelocity = 10;
@@ -25,6 +34,8 @@ int main() {
 	time_t time = clock();
 	time_t delta = (time_t)(1000.0/fps);
 	//cvvWaitKey("Enter");
+	void *fuck=nullptr;
+	setMouseCallback("游戏原画", onMouse, fuck);
 	for (;;) {
 		waitKey(time+delta-clock());
 		height += std::round(velocity);
